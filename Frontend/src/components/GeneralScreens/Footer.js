b29599920e59c84f1962b38231f7311f51f53f29
uday-sv <udaysrivishnu@gmail.com>
2024-07-12 19:52:22 +0530

    adding files to github

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../Css/Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+                {}
+            </div>
+            <div className="copyright">
+                <p className="copyright-blog">© 2024 Uday Suggula, Harshit, Nitin, Uday Kiran. All Rights Reserved</p>
+                <p className="github-link">
+                    <a href="https://github.com/uday-sv/BlogStream" target="_blank" rel="noopener noreferrer">
+                        GitHub Repository
+                    </a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ readme.md
@@ -0,0 +1,1 @@
+BlogStream is a simple blogging platform built with MongoDB, ReactJS, NodeJS, and ExpressJS. It offers robust features for publishing and managing blog content, emphasizing user-friendly CRUD operations and secure authentication.
